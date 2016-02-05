47c0ebaab8be1ac272359e8b1255845838fe745a
Aravindrajvijay <govindrajvijay@gmail.com>
2016-02-05 21:27:14 +0530

    Create sqrt.c

---
+++ sqrt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+double babylonian(int);
+int main(void) 
+{
+    int num;
+    double sq_rt;
+	  printf("Number:");
+   	scanf("%d",&num);
+	  sq_rt=babylonian(num);
+	  printf("\nSquare Root:%f",sq_rt);
+	  return 0;
+}
+double babylonian(int n)
+{
+    double a,b=1.0,accuracy=0.000001;
+    a=n;
+    while((a-b)>accuracy)
+    {
+        a=(a+b)/2;
+        b=n/a;
+    }
+    return a;
+}
+
