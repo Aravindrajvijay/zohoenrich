b2f586704d0d70a0ab276855c146ffb30e1e09f6
Aravindrajvijay <govindrajvijay@gmail.com>
2016-03-11 11:07:27 +0530

    Update Name_count.c

--- Name_count.c
+++ Name_count.c
@@ -1,67 +1,76 @@
 #include <stdio.h>
 #include <string.h>
-struct first_name
+struct details
 {
 	int id;
 	char name[25];
 };
+struct first_name
+{
+    char f_name[25];
+    int index[10];
+    int count;
+};
+
 int main()
 {
-	struct first_name *s;
-	int i,n,j,m=0,len,count,k,index[10];
+	int n,i,j,len,m,k;
 	char temp[25],str[25];
 	printf("\nNumber of students:");
-	scanf("%d",&n);
-	s=(struct first_name*)malloc(sizeof(struct first_name)*n);
+	scanf("%d", &n);
+	struct details *s = (struct details*)malloc(sizeof(struct details) * n);
+	struct first_name *f = (struct first_name*)malloc(sizeof(struct first_name) * n);
 	for(i=0;i<n;i++)
 	{
-		scanf("%d\t",&s[i].id);
-		fgets(s[i].name,25,stdin);
+		scanf("%d\t", &s[i].id);
+		fgets(s[i].name, 25, stdin);
 	}
 	for(i=0;i<n;i++)
 	{
 		strcpy(temp,s[i].name);
-		len=strlen(temp);
+		len = strlen(temp);
 		for(j=0;j<len;j++)
 		{
-			if(temp[j]=='.')
+			if(temp[j] == '.')
 			{
-				m=0;
+				m = 0;
 				continue;
 			}
-			else if(temp[j]==' ')
+			else if(temp[j] == ' ')
 			{
 				break;
 			}
 			else
 			{
-				str[m]=temp[j];
+				str[m] = temp[j];
 				m++;
 			}
 		}
-		str[m]='\0';
+		str[m] = '\0';
 		m=0;
-		strcpy(s[i].name,str);
+		strcpy( f[i].f_name, str);
 	}
 	for(i=0;i<n;i++)
 	{
-		count=1,m=0,k=0;
+	    f[i].count=1;
+		m=0;
+		k=0;
 		for(j=i+1;j<n;j++)
 		{
-			if(strcmp(s[i].name,s[j].name) == 0)
+			if(strcmp(f[i].f_name,f[j].f_name) == 0)
 			{
-				*s[j].name='\0';
-				count++;
-				index[m]=j;
+				*f[j].f_name ='\0';
+				 f[i].count++;
+				*f[i].index = j;
 				m++;
 			}
 		}
-		if(*s[i].name!='\0')
+		if(*f[i].f_name!='\0')
 		{
-			printf("\n%s %d [%d",s[i].name,count,i+1);
+			printf("\n%s %d [%d",f[i].f_name,f[i].count,i+1);
 			while(k<m)
 			{
-				printf(",%d",index[k]+1);
+				printf(",%d",*f[i].index+1);
 				k++;
 			}
 			printf("]");
@@ -69,4 +78,3 @@ int main()
 	}
 	return 0;
 }
-
